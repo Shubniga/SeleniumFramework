d408b2641f6f7783ec6f353b6288d86e16fe4d55
SHUBHAM NIGAM <nigam.shubham7@gmail.com>
2022-04-19 19:37:10 +0530

    Added new steps in feature

--- Cucumberjava/src/test/java/StepDefinitions/YocketSearch.java
+++ Cucumberjava/src/test/java/StepDefinitions/YocketSearch.java
@@ -3,6 +3,7 @@ package StepDefinitions;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,8 +13,8 @@ public class YocketSearch {
 
 	WebDriver Driver = null;
 	
-	@Given("browser is open")
-	public void browser_is_open() {
+	@Given("User is google search page")
+	public void User_is_google_search_page() {
 	    // Write code here that turns the phrase above into concrete actions
 	    System.out.println("Inside step - browser is open");
 	    
@@ -23,32 +24,24 @@ public class YocketSearch {
 	    System.setProperty("webdriver.chrome.driver", projectPath+"/src/test/resources/drivers/chromedriver.exe"); //initial path is similar to projectpath hence appended with variable
 	    Driver = new ChromeDriver();
 	    
+	    Driver.manage().window().maximize(); //maximize window size
 	    Driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15)); //Implicitlywait is deprecated in selenium4 hence used this another workaround
 	    
-	}
-
-	@And("user is google search page")
-	public void user_is_google_search_page() {
 	    // Write code here that turns the phrase above into concrete actions
 		System.out.println("Inside step - user is on google search page");
 		
 		Driver.navigate().to("https://www.google.com/"); //navigating to google webpage
 	}
 
-	@When("user enters yocket.com as search text")
-	public void user_enters_yocket_com_as_search_text() throws InterruptedException {
+	@And("User enters yocket.com as search text hits enter")
+	public void User_enters_yocket_com_as_search_text_hits_enter() throws InterruptedException {
 	    // Write code here that turns the phrase above into concrete actions
 		System.out.println("Inside step - user enters yocket.com as search text");
 		
 		Driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")).sendKeys("yocket.com");
 		Thread.sleep(2000);
-	}
-
-	@And("hits enter")
-	public void hits_enter() throws InterruptedException {
-	    // Write code here that turns the phrase above into concrete actions
+	
 		System.out.println("Inside step - hits enter");
-		
 		Driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")).sendKeys(Keys.ENTER);
 		Thread.sleep(2000);
 	}
@@ -57,6 +50,9 @@ public class YocketSearch {
 	public void user_is_navigated_on_yocket_homepage() {
 	    // Write code here that turns the phrase above into concrete actions
 		System.out.println("Inside step - user is navigated on yocket homepage");
+		
+		JavascriptExecutor js = ((JavascriptExecutor)Driver);
+		js.executeScript("window.scrollTo(0,500)");
 		
 		if(Driver.getPageSource().contains("Yocket: Overseas Education Consultants to Assist You in Your ...")) {
 			System.out.println("Yocket title available");
--- Cucumberjava/target/test-classes/Feature/YocketHomepage.feature
+++ Cucumberjava/target/test-classes/Feature/YocketHomepage.feature
@@ -1,22 +1,19 @@
 #Author: Shubham N
-#Date: 17 April 2022
+#Date: 18 April 2022
 #Description: Validate college finder module
 
-Feature: Feature to test yocket homepage
+Feature: Feature to test yocket college finder module
 
-  Scenario: To validate yocket college finder page
-    Given browser is open
-    And user is google search page
-    When user enters yocket.com as search text
-    And hits enter
+  Scenario: To validate yocket college finder module
+    Given User is google search page
+    And User enters yocket.com as search text hits enter
     Then user is navigated on google search results
     And user selects University College Finder
-    
-Feature: To check if user proceeds further selecting Masters from University College Finder page
-	
-	Scenario: Validate user selects masters
-		Given user is on College University Finder page
-		And user clicks on Masters button
-		Then user is nvaigated to Dream education page
-		
-		
+    Then User selects Masters option
+    And User fills step1 details and moves ahead
+    Then User is navigated to step2
+    And User fills step2 details and moves ahead
+    Then User is navigated to step3 and moves ahead
+    And User fills step3 details and moves ahead
+    Then User is navigated to Step4
+    And User fills step4 details and finds list of universities
